965444319100e5c3b4ccd16615abeab4557f73cc
Sublics <skiski9588@gmail.com>
2025-10-19 13:14:49 +0100

    more improvements

--- main.go
+++ main.go
@@ -3,32 +3,83 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 
 	sysinfo "github.com/elastic/go-sysinfo"
+	"github.com/elastic/go-sysinfo/types"
 )
 
+var (
+	host types.Host
+)
+
+func getHost() {
+	h, err := sysinfo.Host()
+	if err != nil {
+		panic(err)
+	}
+
+	host = h
+}
+
 func getLoggedInUser() string {
-	cmd := exec.Command("whoami")
+	cmd := exec.Command("wmic", "computersystem", "get", "username")
 	output, err := cmd.Output()
 
 	if err != nil {
 		return "Unknown User"
 	}
+	output_lines := strings.Split(string(output), "\n")
 
-	return string(output)
+	return output_lines[1]
 }
 
 func getOs() string {
-	host, err := sysinfo.Host()
-	if err != nil {
-		panic(err)
-	}
+	cmd := exec.Command("wmic", "os", "get", "Caption")
+	output, err := cmd.Output()
 
-	value := host.Info()
-	return fmt.Sprintf("%s %s", string(value.OS.Name), string(value.OS.Version))
+	if err != nil {
+		return "Unknown OS"
+	}
+	output_lines := strings.Split(string(output), "\n")
+
+	return output_lines[1]
+}
+
+func getHostName() string {
+	cmd := exec.Command("wmic", "computersystem", "get", "manufacturer,model")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return "Unknown Computer Model"
+	}
+	output_lines := strings.Split(string(output), "\n")
+
+	return output_lines[1]
+}
+
+func getKernel() string {
+	cmd := exec.Command("wmic", "os", "get", "Version")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return "Unknown Kernel"
+	}
+	output_lines := strings.Split(string(output), "\n")
+
+	return output_lines[1]
+}
+
+func getUptime() string {
+	return "Will work on this later"
 }
 
 func main() {
+	getHost()
 	fmt.Println(getLoggedInUser())
-	fmt.Print(getOs())
+	println()
+	fmt.Println(getOs())
+	fmt.Println(getHostName())
+	fmt.Println(getKernel())
+	fmt.Println(getUptime())
 }

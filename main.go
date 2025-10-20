8763c690ebfc2ff7ee2c15357b71fea083580810
Sublics <skiski9588@gmail.com>
2025-10-20 17:53:39 +0100

    got displaying uptime working (go time library is the best worst thing I've ever worked with)

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os/exec"
 	"strings"
+	"time"
 
 	sysinfo "github.com/elastic/go-sysinfo"
 	"github.com/elastic/go-sysinfo/types"
@@ -70,8 +72,52 @@ func getKernel() string {
 	return output_lines[1]
 }
 
+// makes a word plural if it should be and not if it shouldn't
+func makePlural(quantity int, word string) string {
+	if quantity == 1 {
+		return word
+	}
+	return word + "s"
+}
+
 func getUptime() string {
-	return "Will work on this later"
+	// 						  YYYYMMDDHHMMSS
+	time_layout := "20060102150405"
+	location := time.Now().Location()
+
+	cmd := exec.Command("wmic", "os", "get", "lastbootuptime")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return "Unknown Boot up time"
+	}
+	output_lines := strings.Split(string(output), "\n")
+
+	raw_date := strings.Split(output_lines[1], ".")[0]
+	last_boot_time, err := time.ParseInLocation(time_layout, raw_date, location)
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now()
+	time_since_last_boot := now.Sub(last_boot_time).Seconds()
+
+	hours := int(math.Floor(time_since_last_boot / 3600))
+	time_since_last_boot -= float64(hours) * 3600
+	minutes := int(math.Floor(time_since_last_boot / 60))
+	time_since_last_boot -= float64(minutes) * 60
+
+	var hour = makePlural(hours, "hour")
+	var min = makePlural(minutes, "min")
+
+	var date_str string
+	if hours > 0 {
+		date_str = fmt.Sprintf("%d %s, %d %s", hours, hour, minutes, min)
+	} else {
+		date_str = fmt.Sprintf("%d %s", minutes, min)
+	}
+
+	return date_str
 }
 
 func main() {
